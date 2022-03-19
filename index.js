0a52468663ed5ab046082728bde6c2585815bde1
Nilton <niltonroese@gmail.com>
2022-03-19 17:24:17 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+
+//document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "NILS is the champion"
+
+/*
+<h1></h1>
+<h1 id="victory">H1</h1>
+<h1 id="victory">NILS is the champion</h1>
+*/ 
